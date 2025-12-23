304e15cd4e3af6db854249e3bb6ba2aef8928d92
Twaish <axiomeverything@gmail.com>
2025-12-23 15:08:27 +0100

    Remove unnecessary constants in i18n translations

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -2,23 +2,18 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import pkg from '@/../package.json'
 
-const constants = {
-  appName: pkg.productName,
-}
-
 i18n.use(initReactI18next).init({
   fallbackLng: 'en',
   resources: {
     en: {
       translation: {
-        ...constants,
+        appName: pkg.productName,
         titleHomePage: 'Home Page',
         titleCounterPage: 'Counter Page',
       },
     },
     'da-DK': {
       translation: {
-        ...constants,
         titleHomePage: 'Hjem Side',
         titleCounterPage: 'Tæller Side',
       },
