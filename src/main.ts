cb0e8f4361da9f283838a62e8fd7a69836053f15
Twaish <axiomeverything@gmail.com>
2025-12-23 13:16:56 +0100

    Add Drizzle ORM

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-import { app, BrowserWindow } from "electron";
-import registerListeners from "./helpers/ipc/listeners-register";
-// "electron-squirrel-startup" seems broken when packaging with vite
-//import started from "electron-squirrel-startup";
-import path from "path";
+import { app, BrowserWindow } from 'electron'
+import registerListeners from './helpers/ipc/listeners-register'
+import path from 'path'
 import {
   installExtension,
   REACT_DEVELOPER_TOOLS,
-} from "electron-devtools-installer";
+} from 'electron-devtools-installer'
+import 'dotenv/config'
+import { drizzle } from 'drizzle-orm/libsql'
 
-const inDevelopment = process.env.NODE_ENV === "development";
+const db = drizzle(process.env.DB_FILE_NAME!)
+
+const inDevelopment = process.env.NODE_ENV === 'development'
 
 function createWindow() {
-  const preload = path.join(__dirname, "preload.js");
+  const preload = path.join(__dirname, 'preload.js')
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -23,40 +25,40 @@ function createWindow() {
 
       preload: preload,
     },
-    titleBarStyle: "hidden",
-  });
-  registerListeners(mainWindow);
+    titleBarStyle: 'hidden',
+  })
+  registerListeners(mainWindow, db)
 
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL)
   } else {
     mainWindow.loadFile(
       path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
-    );
+    )
   }
 }
 
 async function installExtensions() {
   try {
-    const result = await installExtension(REACT_DEVELOPER_TOOLS);
-    console.log(`Extensions installed successfully: ${result.name}`);
+    const result = await installExtension(REACT_DEVELOPER_TOOLS)
+    console.log(`Extensions installed successfully: ${result.name}`)
   } catch {
-    console.error("Failed to install extensions");
+    console.error('Failed to install extensions')
   }
 }
 
-app.whenReady().then(createWindow).then(installExtensions);
+app.whenReady().then(createWindow).then(installExtensions)
 
 //osX only
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-});
+})
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
+    createWindow()
   }
-});
+})
 //osX only ends
--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from "vite";
-import path from "path";
+import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vitejs.dev/config
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['libsql', '@libsql/client', '@libsql/win32-x64-msvc'],
+    },
+  },
+  optimizeDeps: {
+    exclude: ['libsql', '@libsql/client', '@libsql/win32-x64-msvc'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    }
-  }
-});
+    },
+  },
+})
