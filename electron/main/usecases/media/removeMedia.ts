c79d09a1beab3032b67e49cd8340c6505ac33bd3
Twaish <axiomeverything@gmail.com>
2026-01-11 20:27:52 +0100

    Add removeMedia use case

---
+++ electron/main/usecases/media/removeMedia.ts
@@ -0,0 +1,10 @@
+import { MediaRepository } from '@/domain/repositories/mediaRepository'
+
+export default class RemoveMedia {
+  constructor(private readonly repo: MediaRepository) {}
+
+  execute(mediaIds: number[]) {
+    if (!mediaIds.length) return { deleted: 0, ids: [] }
+    return this.repo.remove(mediaIds)
+  }
+}
