b131b4bdbc7ad13acc6eb07ed39e576bddf660c4
Twaish <axiomeverything@gmail.com>
2026-01-11 20:27:46 +0100

    Add addMedia use case

---
+++ electron/main/usecases/media/addMedia.ts
@@ -0,0 +1,25 @@
+import StorageService from '@/core/StorageService'
+import { MediaRepository } from '@/domain/repositories/mediaRepository'
+import { MediaCreateInput } from '@shared/types'
+
+export default class AddMedia {
+  constructor(
+    private readonly repo: MediaRepository,
+    private readonly storage: StorageService,
+  ) {}
+
+  async execute(media: MediaCreateInput) {
+    let thumbnail: string | undefined
+
+    if (media.thumbnail) {
+      const stored = await this.storage.storeImage(media.thumbnail)
+      thumbnail = stored.relativePath
+    }
+
+    const mediaToAdd = {
+      ...media,
+      thumbnail,
+    }
+    return this.repo.add(mediaToAdd)
+  }
+}
