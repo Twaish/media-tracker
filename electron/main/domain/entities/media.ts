8d83090bfe5a1d6b0ede02bf6d4af65d129d7b3c
Twaish <axiomeverything@gmail.com>
2026-01-11 20:26:34 +0100

    Made Media constructor private. Move Media params into props object

--- electron/main/domain/entities/media.ts
+++ electron/main/domain/entities/media.ts
@@ -10,20 +10,99 @@ export const MEDIA_STATUS = [
 export type MediaType = (typeof MEDIA_TYPES)[number]
 export type MediaStatus = (typeof MEDIA_STATUS)[number]
 
+type MediaParams = {
+  id: number | null
+  title: string
+  currentEpisode: number
+  maxEpisodes: number | null
+  thumbnail: string | null
+  mediaType: MediaType
+  status: MediaStatus
+  externalLink: string
+  alternateTitles: string
+  watchAfter: number | null
+  lastUpdated: Date | null
+  createdAt: Date | null
+  isFavorite: boolean
+}
+
 export class Media {
-  constructor(
-    public readonly id: number | null,
-    public readonly title: string,
-    public readonly currentEpisode: number,
-    public readonly maxEpisodes: number,
-    public readonly thumbnail: string,
-    public readonly mediaType: MediaType,
-    public readonly status: MediaStatus,
-    public readonly externalLink: string,
-    public readonly alternateTitles: string,
-    public readonly watchAfter: number,
-    public readonly lastUpdated: number,
-    public readonly createdAt: number,
-    public readonly isFavorite: boolean,
-  ) {}
+  private constructor(private readonly props: MediaParams) {}
+
+  static create(params: Omit<MediaParams, 'id'>): Media {
+    const {
+      title,
+      currentEpisode = 0,
+      maxEpisodes,
+      thumbnail,
+      mediaType = 'anime',
+      status = 'plan-to-watch',
+      externalLink = '/',
+      alternateTitles = '',
+      watchAfter,
+      lastUpdated = new Date(),
+      createdAt = new Date(),
+      isFavorite,
+    } = params
+
+    return new Media({
+      id: null,
+      title,
+      currentEpisode,
+      maxEpisodes,
+      thumbnail,
+      mediaType,
+      status,
+      externalLink,
+      alternateTitles,
+      watchAfter,
+      lastUpdated,
+      createdAt,
+      isFavorite,
+    })
+  }
+
+  withId(id: number) {
+    return new Media({ ...this.props, id })
+  }
+
+  get id() {
+    return this.props.id
+  }
+  get title() {
+    return this.props.title
+  }
+  get currentEpisode() {
+    return this.props.currentEpisode
+  }
+  get maxEpisodes() {
+    return this.props.maxEpisodes
+  }
+  get thumbnail() {
+    return this.props.thumbnail
+  }
+  get mediaType() {
+    return this.props.mediaType
+  }
+  get status() {
+    return this.props.status
+  }
+  get externalLink() {
+    return this.props.externalLink
+  }
+  get alternateTitles() {
+    return this.props.alternateTitles
+  }
+  get watchAfter() {
+    return this.props.watchAfter
+  }
+  get lastUpdated() {
+    return this.props.lastUpdated
+  }
+  get createdAt() {
+    return this.props.createdAt
+  }
+  get isFavorite() {
+    return this.props.isFavorite
+  }
 }
