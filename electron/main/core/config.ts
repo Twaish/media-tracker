cf06005f94837efcfabf5f1ae6cf27af61369c52
Twaish <axiomeverything@gmail.com>
2026-01-25 03:24:25 +0100

    Add file path for log file

--- electron/main/core/config.ts
+++ electron/main/core/config.ts
@@ -3,7 +3,9 @@ import path from 'path'
 
 const userData = app.getPath('userData')
 const databaseFileName = 'local.db'
+const logFileName = 'app.log'
 
 export default {
   DB_PATH: `file:${path.join(userData, databaseFileName)}`,
+  LOG_PATH: path.join(userData, logFileName),
 }
