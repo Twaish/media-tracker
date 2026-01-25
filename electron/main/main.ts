9928f98191b65b455bcd44c9623dd03a0b52dfa2
Twaish <axiomeverything@gmail.com>
2026-01-25 03:34:58 +0100

    Use WinstonLogger for logging. Add try/catch to initialization

--- electron/main/main.ts
+++ electron/main/main.ts
@@ -1,38 +1,59 @@
 import 'dotenv/config'
 import { app, BrowserWindow } from 'electron'
+import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql'
 
+import config from './core/config'
 import StorageService from './core/StorageService'
 import { ElectronWindow } from './core/ElectronWindow'
+
 import { seedDatabase } from './db/seeding'
-import registerListeners from './helpers/ipc/listeners-register'
-import registerProtocols from './helpers/ipc/protocols-register'
-import config from './core/config'
-import { createClient } from '@libsql/client'
 import { runMigrations } from './db/migrate'
 
+import registerListeners from './helpers/ipc/listeners-register'
+import registerProtocols from './helpers/ipc/protocols-register'
+
+import { consoleFormat, fileFormat } from './core/logger/formats'
+import {
+  createConsoleTransport,
+  createFileTransport,
+} from './core/logger/transports'
+import { WinstonLogger } from './core/logger'
+
 app.whenReady().then(async () => {
-  console.log('INITIALIZING')
+  const { DB_PATH, LOG_PATH } = config
 
-  const dbClient = createClient({ url: config.DB_PATH })
-  const database = drizzle(dbClient)
-  const electronWindow = new ElectronWindow()
-  const storageService = new StorageService('./Media Images')
+  const consoleTransport = createConsoleTransport(consoleFormat)
+  const fileTransport = createFileTransport(LOG_PATH, fileFormat)
+  const logger = new WinstonLogger([consoleTransport, fileTransport])
+  logger.header('Initializing')
+  try {
+    const dbClient = createClient({ url: DB_PATH })
+    const database = drizzle(dbClient)
+    const electronWindow = new ElectronWindow()
+    const storageService = new StorageService('./Media Images')
 
-  const modules = {
-    ElectronWindow: electronWindow,
-    window: electronWindow.window,
-    Database: database,
-    StorageService: storageService,
+    const modules = {
+      ElectronWindow: electronWindow,
+      window: electronWindow.window,
+      Database: database,
+      StorageService: storageService,
+    }
+
+    registerListeners(modules)
+    registerProtocols(modules)
+
+    await runMigrations(database)
+    await seedDatabase(database)
+
+    electronWindow.showWindow()
+  } catch (err) {
+    logger.header('Fatal Error')
+    logger.error(
+      `Application failed during initialization: ${err instanceof Error ? err : String(err)}`,
+    )
+    app.quit()
   }
-
-  registerListeners(modules)
-  registerProtocols(modules)
-
-  await runMigrations(database)
-  await seedDatabase(database)
-
-  electronWindow.showWindow()
 })
 
 //osX only
