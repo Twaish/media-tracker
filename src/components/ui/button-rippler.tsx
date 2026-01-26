d2e1204d84ba05dfe1153e93d9b3507ab3d07dd2
Twaish <axiomeverything@gmail.com>
2026-01-26 18:01:16 +0100

    Add button rippler to button component

---
+++ src/components/ui/button-rippler.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from 'react'
+import './button-rippler.css'
+
+export const Rippler = ({
+  buttonRef,
+  duration = 500,
+}: {
+  buttonRef: React.RefObject<HTMLButtonElement | null>
+  duration?: number
+}): React.ReactNode => {
+  const ripplerRef = useRef<HTMLDivElement>(null)
+  const handleClick = (e: MouseEvent) => {
+    const button = buttonRef.current
+    const rippler = ripplerRef.current
+    if (!button || !rippler) {
+      return
+    }
+    const rect = button.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    const ripple = document.createElement('span')
+    ripple.classList.add('ripple')
+    ripple.className =
+      'absolute pointer-events-none rounded-full bg-current opacity-30 animate-[ripple] duration-[var(--anim-time)]'
+    ripple.style.setProperty('--anim-time', `${duration}ms`)
+    ripple.style.left = x + 'px'
+    ripple.style.top = y + 'px'
+    ripple.style.transform = 'translate(-50%, -50%)'
+    rippler.appendChild(ripple)
+
+    setTimeout(() => {
+      ripple.remove()
+    }, duration)
+  }
+
+  useEffect(() => {
+    const button = buttonRef?.current
+    console.log(button)
+    if (!button) {
+      return
+    }
+    button.addEventListener('click', handleClick)
+
+    return () => {
+      button.removeEventListener('click', handleClick)
+    }
+  }, [buttonRef])
+
+  if (!buttonRef) return
+
+  return (
+    <div
+      ref={ripplerRef}
+      className={'absolute inset-0 overflow-hidden rounded-[inherit]'}
+    ></div>
+  )
+}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,9 +3,10 @@ import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/utils/tailwind'
+import { Rippler } from './button-rippler'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background cursor-pointer transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background cursor-pointer transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative',
   {
     variants: {
       variant: {
@@ -40,14 +41,19 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
+    const localRef = React.useRef<HTMLButtonElement>(null)
+    React.useImperativeHandle(ref, () => localRef.current!)
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
+        ref={localRef}
         {...props}
-      />
+      >
+        {children}
+        <Rippler buttonRef={localRef} />
+      </Comp>
     )
   },
 )
