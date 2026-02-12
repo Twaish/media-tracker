f1b25bc5bf1452d1f88fbb04b7b11e891a492443
Twaish <axiomeverything@gmail.com>
2026-02-12 01:33:15 +0100

    Use repository search function in resolveSearchQuery

--- electron/main/usecases/search/index.ts
+++ electron/main/usecases/search/index.ts
@@ -1,10 +1,13 @@
 import { QueryResolver } from '@/domain/services/QueryResolver'
 import ResolveSearchQuery from './resolveSearchQuery'
+import { Modules } from '@/helpers/ipc/types'
+import { MediaRepositoryDrizzle } from '@/infrastructure/db/repositories/mediaRepositoryDrizzle'
 
-export function createSearchUseCases() {
+export function createSearchUseCases({ Database }: Modules) {
+  const repo = new MediaRepositoryDrizzle(Database)
   const resolver = new QueryResolver()
 
   return {
-    resolveSearchQuery: new ResolveSearchQuery(resolver),
+    resolveSearchQuery: new ResolveSearchQuery(repo, resolver),
   }
 }
--- electron/main/usecases/search/resolveSearchQuery.ts
+++ electron/main/usecases/search/resolveSearchQuery.ts
@@ -1,9 +1,19 @@
+import { IMediaRepository } from '@/application/db/repositories/IMediaRepository'
 import { QueryResolver } from '@/domain/services/QueryResolver'
 
+// TODO: Change this into a SearchMedia use case instead
 export default class ResolveSearchQuery {
-  constructor(private readonly resolver: QueryResolver) {}
+  constructor(
+    private readonly repo: IMediaRepository,
+    private readonly resolver: QueryResolver,
+  ) {}
 
   async execute(query: string) {
-    return this.resolver.resolve(query)
+    const { title, filters } = this.resolver.resolve(query)
+
+    return this.repo.search({
+      title,
+      filters,
+    })
   }
 }
