223c027814ca816426e90cdbeeeb6a4486c5cf96
Twaish <axiomeverything@gmail.com>
2026-02-12 01:31:53 +0100

    Add search function to IMediaRepository interface. Move results from get by pagination into MediaPaginationResult type

--- electron/main/application/db/repositories/IMediaRepository.ts
+++ electron/main/application/db/repositories/IMediaRepository.ts
@@ -4,19 +4,29 @@ import {
   MediaUpdateInput,
 } from '@shared/types'
 import { Media } from '@/domain/entities/media'
+import { Filter } from '@/domain/services/QueryResolver'
+
+export type MediaPaginationResult = {
+  data: Media[]
+  pagination: {
+    page: number
+    limit: number
+    totalPages: number
+    totalItems: number
+  }
+}
 
 export interface IMediaRepository {
   getById(id: number): Promise<Media>
-  getWithPagination(options: MediaPaginationOptions): Promise<{
-    data: Media[]
-    pagination: {
-      page: number
-      limit: number
-      totalPages: number
-      totalItems: number
-    }
-  }>
+  getWithPagination(
+    options: MediaPaginationOptions,
+  ): Promise<MediaPaginationResult>
   add(media: MediaCreateInput): Promise<Media>
   remove(mediaIds: number[]): Promise<{ deleted: number; ids: number[] }>
   update(media: MediaUpdateInput): Promise<Media>
+  search(options: {
+    title?: string
+    filters?: Filter[]
+    pagination?: MediaPaginationOptions
+  }): Promise<MediaPaginationResult>
 }
