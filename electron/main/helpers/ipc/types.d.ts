190cd6fc1faece7d6e556fa9dd13eccdf01b9063
Twaish <axiomeverything@gmail.com>
2026-02-17 13:30:20 +0100

    Remove Database and add Media- & GenresRepository from Modules

--- electron/main/helpers/ipc/types.d.ts
+++ electron/main/helpers/ipc/types.d.ts
@@ -1,15 +1,20 @@
-import { IAiService } from '@/application/ai/IAiService'
-import { ILogger } from '@/application/logging/ILogger'
+import { BrowserWindow } from 'electron'
+
 import { ElectronWindow } from '@/core/ElectronWindow'
 import { StorageService } from '@/core/StorageService'
-import { LibSQLDatabase } from 'drizzle-orm/libsql'
-import { BrowserWindow } from 'electron'
+
+import { IAiService } from '@/application/ai/IAiService'
+import { ILogger } from '@/application/logging/ILogger'
+import { IGenresRepository } from '@/application/db/repositories/IGenresRepository'
+import { IMediaRepository } from '@/application/db/repositories/IMediaRepository'
 
 declare interface Modules {
   ElectronWindow: ElectronWindow
   StorageService: StorageService
-  Database: LibSQLDatabase
   window: BrowserWindow
   logger: ILogger
   AiService: IAiService
+
+  MediaRepository: IMediaRepository
+  GenresRepository: IGenresRepository
 }
