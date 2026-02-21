3af4ee8fd31449286c39b55bcc4636fdf2b7fb96
Twaish <axiomeverything@gmail.com>
2026-02-21 20:09:03 +0100

    Add JsonStore

---
+++ electron/main/core/JsonStore.ts
@@ -0,0 +1,57 @@
+import { app } from 'electron'
+import path from 'path'
+import fs from 'fs/promises'
+import { existsSync, mkdirSync } from 'fs'
+import EventEmitter from 'events'
+
+export class JsonStore extends EventEmitter {
+  basePath: string
+  fullPath: string
+  constructor(basePath: string = '') {
+    super()
+    this.basePath = basePath
+    this.fullPath = path.join(app.getPath('userData'), this.basePath)
+    if (!existsSync(this.fullPath)) {
+      mkdirSync(this.fullPath, { recursive: true })
+    }
+  }
+
+  private getSafePath(filename: string): string {
+    const resolvedPath = path.join(this.fullPath, `${filename}.json`)
+    const normalizedRelative = path.relative(this.fullPath, resolvedPath)
+
+    if (
+      normalizedRelative.startsWith('..') ||
+      path.isAbsolute(normalizedRelative)
+    ) {
+      throw new Error('Invalid filename: Path traversal detected.')
+    }
+    return resolvedPath
+  }
+
+  async get<T>(filename: string): Promise<T | null> {
+    try {
+      const filePath = this.getSafePath(filename)
+      const data = await fs.readFile(filePath, 'utf-8')
+      const parsedData = JSON.parse(data) as T
+      return parsedData
+    } catch (err) {
+      return null
+    }
+  }
+
+  async set(filename: string, value: unknown): Promise<void> {
+    const filePath = this.getSafePath(filename)
+    const content = JSON.stringify(value)
+    await fs.writeFile(filePath, content)
+    this.emit('changed', filename, filePath, content)
+  }
+
+  async remove(filename: string): Promise<void> {
+    const filePath = this.getSafePath(filename)
+    try {
+      await fs.unlink(filePath)
+      this.emit('removed', filename, filePath)
+    } catch {}
+  }
+}
