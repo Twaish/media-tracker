fb30d7904ac02e56e568b7744619f1d0c6aba5bb
Twaish <axiomeverything@gmail.com>
2026-02-22 23:21:51 +0100

    Remove unused types in genres.d.ts

--- shared/types/genres.d.ts
+++ shared/types/genres.d.ts
@@ -1,9 +1,5 @@
-import { genresTable } from '@/infrastructure/db/schema'
-import { InferInsertModel, InferSelectModel } from 'drizzle-orm'
-
-export type Genre = InferSelectModel<typeof genresTable>
-export type GenreCreateInput = InferInsertModel<typeof genresTable>
+import { PersistedGenre } from '@/domain/entities/genre'
 
 export interface GenresContext {
-  get: () => Promise<Genre[]>
+  get: () => Promise<PersistedGenre[]>
 }
