35b73b1ee381184c2a711023c6be6e7f97f7e98f
Twaish <axiomeverything@gmail.com>
2026-02-22 19:50:41 +0100

    Add Executor type

--- electron/main/infrastructure/db/types.d.ts
+++ electron/main/infrastructure/db/types.d.ts
@@ -1,4 +1,15 @@
 import { LibSQLDatabase } from 'drizzle-orm/libsql'
 import * as schema from './schema'
+import { SQLiteTransaction } from 'drizzle-orm/sqlite-core'
+import { ResultSet } from '@libsql/client'
+import { ExtractTablesWithRelations } from 'drizzle-orm'
 
 export type DrizzleDb = LibSQLDatabase<typeof schema>
+export type Executor =
+  | DrizzleDb
+  | SQLiteTransaction<
+      'async',
+      ResultSet,
+      typeof schema,
+      ExtractTablesWithRelations<typeof schema>
+    >
