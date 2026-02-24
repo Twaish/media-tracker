d658ed82253c4a581385b76ae287c5aa0651c7b6
Twaish <axiomeverything@gmail.com>
2026-02-25 00:17:43 +0100

    Index properties in media table
    - Index externalLink, thumbnail, title, watchAfter in media table to increase performance

--- electron/main/infrastructure/db/tables/media.table.ts
+++ electron/main/infrastructure/db/tables/media.table.ts
@@ -1,5 +1,6 @@
 import { MediaStatus, MediaType } from '@/domain/entities/media'
 import { sql } from 'drizzle-orm'
+import { index } from 'drizzle-orm/sqlite-core'
 import {
   AnySQLiteColumn,
   int,
@@ -7,24 +8,35 @@ import {
   text,
 } from 'drizzle-orm/sqlite-core'
 
-export const mediaTable = sqliteTable('media', {
-  id: int().primaryKey({ autoIncrement: true }),
-  title: text().notNull(),
-  currentEpisode: int().notNull().default(0),
-  maxEpisodes: int(),
-  thumbnail: text(),
+export const mediaTable = sqliteTable(
+  'media',
+  {
+    id: int().primaryKey({ autoIncrement: true }),
+    title: text().notNull(),
+    currentEpisode: int().notNull().default(0),
+    maxEpisodes: int(),
+    thumbnail: text(),
 
-  type: text().$type<MediaType>().notNull().default('anime'),
-  status: text().$type<MediaStatus>().notNull().default('plan-to-watch'),
+    type: text().$type<MediaType>().notNull().default('anime'),
+    status: text().$type<MediaStatus>().notNull().default('plan-to-watch'),
 
-  externalLink: text(),
-  alternateTitles: text(),
+    externalLink: text(),
+    alternateTitles: text(),
 
-  watchAfter: int().references((): AnySQLiteColumn => mediaTable.id),
+    watchAfter: int().references((): AnySQLiteColumn => mediaTable.id),
 
-  lastUpdated: int({ mode: 'timestamp_ms' }).default(sql`(unixepoch() * 1000)`),
-  createdAt: int({ mode: 'timestamp_ms' }).default(sql`(unixepoch() * 1000)`),
-  isFavorite: int({ mode: 'boolean' }).notNull().default(false),
+    lastUpdated: int({ mode: 'timestamp_ms' }).default(
+      sql`(unixepoch() * 1000)`,
+    ),
+    createdAt: int({ mode: 'timestamp_ms' }).default(sql`(unixepoch() * 1000)`),
+    isFavorite: int({ mode: 'boolean' }).notNull().default(false),
 
-  deletedAt: int({ mode: 'timestamp_ms' }),
-})
+    deletedAt: int({ mode: 'timestamp_ms' }),
+  },
+  (table) => ({
+    externalLinkIdx: index('idx_media_external_link').on(table.externalLink),
+    thumbnailIdx: index('idx_media_thumbnail').on(table.thumbnail),
+    titleIdx: index('idx_media_title').on(table.title),
+    watchAfterIdx: index('idx_media_watch_after').on(table.watchAfter),
+  }),
+)
