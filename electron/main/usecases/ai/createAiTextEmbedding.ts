de503d13a74999cec238de17d1f28ef9bbb8ed7b
Twaish <axiomeverything@gmail.com>
2026-02-25 01:04:59 +0100

    Add CreateAiTextEmbedding use case

---
+++ electron/main/usecases/ai/createAiTextEmbedding.ts
@@ -0,0 +1,9 @@
+import { IAiService } from '@/application/ai/IAiService'
+
+export default class CreateAiTextEmbedding {
+  constructor(private readonly aiService: IAiService) {}
+
+  async execute(text: string, model?: string) {
+    return this.aiService.embed(text, model)
+  }
+}
