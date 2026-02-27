34131f51335df9188437bacba4e53beb6ce9e3ac
Twaish <axiomeverything@gmail.com>
2026-02-27 17:12:30 +0100

    Add doc to compile function for RuleEngine

--- electron/main/domain/automation/RuleEngine.ts
+++ electron/main/domain/automation/RuleEngine.ts
@@ -4,6 +4,45 @@ import { SemanticAnalyzer } from './SemanticAnalyzer'
 import { Rule, Template } from './types'
 
 export class RuleEngine {
+  /**
+   * How to define a rule:
+   * ```
+   * RULE <name>
+   * ON <target> <field/expression> <operator> <field/expression>
+   * [PRIORITY <number>]
+   * DO { <actions> }
+   * ```
+   *
+   * How to define a template:
+   * ```
+   * TEMPLATE <name>
+   * PARAMETERS { <param> [<param>, ...] }
+   * DO { <actions> }
+   * ```
+   *
+   * ### Actions:
+   *
+   * ```
+   * set <field> = <expression>
+   * ```
+   * ```
+   * add <field> = <expression>
+   * ```
+   * ```
+   * call template("<name>")
+   * ```
+   * ```
+   * call plugin("<name>")
+   * ```
+   * ```
+   * call http(<url/expression>) {
+   *    METHOD <get|post|put|patch|delete>
+   *    BODY { <name>: <expression> [<name>: <expression>, ...] }
+   *    HEADERS { <name>: <expression> [<name>: <expression>, ...] }
+   *    RETRY <attempts> EXPONENTIAL DELAY <ms>
+   * }
+   * ```
+   */
   compile(source: string): Template | Rule {
     const lexer = new Lexer(source)
     const tokens = lexer.tokenize()
