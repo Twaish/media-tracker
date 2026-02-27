5012624779518f669a431ec079267094543fa051
Twaish <axiomeverything@gmail.com>
2026-02-27 16:34:44 +0100

    Define types for RuleEngine

---
+++ electron/main/domain/automation/types.ts
@@ -0,0 +1,153 @@
+export type Expression =
+  | LiteralExpression
+  | VariableExpression
+  | FieldExpression
+  | FunctionExpression
+  | ObjectExpression
+  | SelfExpression
+
+export type LiteralExpression = {
+  type: 'literal'
+  value: string | number | boolean
+}
+
+export type VariableExpression = {
+  type: 'variable'
+  path: string
+}
+
+export type FieldExpression = {
+  type: 'field'
+  name: string
+}
+
+export type FunctionExpression = {
+  type: 'function'
+  name: string
+  args: Expression[]
+}
+
+export type ObjectExpression = {
+  type: 'object'
+  value: Record<string, Expression>
+}
+
+export type SelfExpression = {
+  type: 'self'
+}
+
+export type Action =
+  | HttpAction
+  | SetAction
+  | AppendAction
+  | TemplateAction
+  | PluginAction
+
+export const HttpMethods = ['GET', 'POST', 'PUT', 'DELETE'] as const
+
+export type HttpMethod = (typeof HttpMethods)[number]
+
+export type HttpAction = {
+  type: 'http'
+  url: Expression
+  method: HttpMethod
+  body?: Expression
+  headers?: Expression
+  retry?: {
+    attempts: number
+    strategy: 'exponential'
+    delayMs: number
+  }
+}
+
+export type SetAction = {
+  type: 'set'
+  field: string
+  value: Expression
+}
+
+export type AppendAction = {
+  type: 'append'
+  field: string
+  value: Expression
+}
+
+export type TemplateAction = {
+  type: 'template'
+  name: string
+  args: Record<string, Expression>
+}
+
+export type PluginAction = {
+  type: 'plugin'
+  name: string
+  args: Record<string, Expression>
+}
+
+export enum TokenType {
+  Identifier = 'identifier',
+  Number = 'number',
+  String = 'string',
+  Operator = 'operator',
+  LParen = 'lparen',
+  RParen = 'rparen',
+  LBrace = 'lbrace',
+  RBrace = 'rbrace',
+  Comma = 'comma',
+  Dot = '.',
+  Keyword = 'keyword',
+  EOF = 'eof',
+}
+
+export type Condition = BinaryExpression
+
+export type BinaryExpression = {
+  type: 'binary'
+  operator: '>' | '<' | '>=' | '<=' | '==' | '!='
+  left: Expression
+  right: Expression
+}
+
+export type Template = {
+  type: 'template'
+  name: string
+  parameters: string[]
+  requires?: {
+    config?: string[]
+    secrets?: string[]
+  }
+  actions: Action[]
+}
+
+export type Rule = {
+  type: 'rule'
+  id: string
+  priority: number
+  enabled: boolean
+  condition: Condition
+  execution: 'sequential'
+  actions: Action[]
+}
+
+export type Token = {
+  type: TokenType
+  value: string
+}
+
+export const KEYWORDS = new Set([
+  'TEMPLATE',
+  'PARAMETERS',
+  'DO',
+  'ON',
+  'PRIORITY',
+  'METHOD',
+  'BODY',
+  'HEADERS',
+  'RETRY',
+  'EXPONENTIAL',
+  'DELAY',
+  'call',
+  // 'http',
+  'set',
+  'add',
+])
