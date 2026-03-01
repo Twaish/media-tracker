607224f6cb9fb7e534504573563c744826160093
Twaish <axiomeverything@gmail.com>
2026-03-01 02:02:42 +0100

    Add IRuleRepository

---
+++ electron/main/application/db/repositories/IRuleRepository.ts
@@ -0,0 +1,10 @@
+import { PersistedRule } from '@/domain/entities/rule'
+import { AddRuleDTO, UpdateRuleDTO } from '@shared/types/rules'
+
+export interface IRuleRepository {
+  getById(id: number): Promise<PersistedRule>
+  add(rule: AddRuleDTO): Promise<PersistedRule>
+  remove(ids: number[]): Promise<{ deleted: number; ids: number[] }>
+  update(rule: UpdateRuleDTO): Promise<PersistedRule>
+  getAllEnabled(): Promise<PersistedRule[]>
+}
