99b1cd968b27c7a24e98e8f588cf0298c636a03f
Twaish <axiomeverything@gmail.com>
2026-03-01 19:07:46 +0100

    Add function for initializing automation use cases

---
+++ electron/main/usecases/automation/index.ts
@@ -0,0 +1,26 @@
+import { Modules } from '@/helpers/ipc/types'
+import AddRule from './addRule'
+import UpdateRule from './updateRule'
+import RemoveRules from './removeRules'
+import GetEnabledRules from './getEnabledRules'
+import AddTemplate from './addTemplate'
+import UpdateTemplate from './updateTemplate'
+import RemoveTemplates from './removeTemplates'
+import GetAllTemplates from './getAllTemplates'
+
+export function createAutomationUseCases({
+  RuleRepository,
+  TemplateRepository,
+}: Modules) {
+  return {
+    addRule: new AddRule(RuleRepository),
+    updateRule: new UpdateRule(RuleRepository),
+    removeRules: new RemoveRules(RuleRepository),
+    getEnabledRules: new GetEnabledRules(RuleRepository),
+
+    addTemplate: new AddTemplate(TemplateRepository),
+    updateTemplate: new UpdateTemplate(TemplateRepository),
+    removeTemplates: new RemoveTemplates(TemplateRepository),
+    getAllTemplates: new GetAllTemplates(TemplateRepository),
+  }
+}
