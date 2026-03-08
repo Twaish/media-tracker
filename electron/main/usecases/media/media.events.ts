80a561571fbbcc330c533b0cbf01de124890ab57
Twaish <axiomeverything@gmail.com>
2026-03-08 13:44:27 +0100

    Change MEDIA_UPDATED. Add Media event definitions. Add functions
    - Change MEDIA_UPDATED string value to mediaUpdated instead of media.updated as the parser will treat it as a nested property
    - Add Media event definitions
    - Add functions to subscribe media events and add media events to event registry

--- electron/main/usecases/media/media.events.ts
+++ electron/main/usecases/media/media.events.ts
@@ -1,10 +1,31 @@
 import { PersistedMedia } from '@/domain/entities/media'
+import { Modules } from '@/helpers/ipc/types'
 
 export enum MEDIA_EVENTS {
-  MEDIA_UPDATED = 'media.updated',
+  MEDIA_UPDATED = 'mediaUpdated',
 }
+
+export const mediaEventDefinitions = [
+  {
+    name: MEDIA_EVENTS.MEDIA_UPDATED,
+    description: 'Triggered when a media entry is updated',
+  },
+]
 
 export type MediaUpdatedPayload = {
   current: PersistedMedia
   previous: PersistedMedia
+}
+
+export function subscribeToMediaEvents({ EventBus, RuleEngine }: Modules) {
+  EventBus.subscribe(
+    MEDIA_EVENTS.MEDIA_UPDATED,
+    async (payload: MediaUpdatedPayload) => {
+      await RuleEngine.handle(MEDIA_EVENTS.MEDIA_UPDATED, payload)
+    },
+  )
+}
+
+export function registerMediaEvents({ EventRegistry }: Modules) {
+  mediaEventDefinitions.map(EventRegistry.register)
 }
