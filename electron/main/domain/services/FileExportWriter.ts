a6ad3d94356a7baacc81388d16a11c397cd8c52a
Twaish <axiomeverything@gmail.com>
2026-03-11 22:38:34 +0100

    Add FileExportWriter

---
+++ electron/main/domain/services/FileExportWriter.ts
@@ -0,0 +1,45 @@
+import fs from 'fs'
+
+export class FileExportWriter {
+  private stream!: fs.WriteStream
+  private firstArray = true
+  private firstItem = true
+
+  async open(path: string) {
+    if (fs.existsSync(path)) {
+      throw new Error(`${path} already exists`)
+    }
+    this.stream = fs.createWriteStream(path)
+  }
+
+  async beginObject() {
+    this.stream.write('{')
+  }
+
+  async endObject() {
+    this.stream.write('}')
+  }
+
+  async beginArray(name: string) {
+    if (!this.firstArray) this.stream.write(',')
+    this.firstArray = false
+
+    this.stream.write(`"${name}":[`)
+    this.firstItem = true
+  }
+
+  async writeItem(item: unknown) {
+    if (!this.firstItem) this.stream.write(',')
+    this.firstItem = false
+
+    this.stream.write(JSON.stringify(item))
+  }
+
+  async endArray() {
+    this.stream.write(']')
+  }
+
+  async close() {
+    this.stream.end()
+  }
+}
