409b878ee3e89f48f7203e2a5fd8fa5e554d366c
Twaish <axiomeverything@gmail.com>
2026-03-12 17:31:39 +0100

    Add JSdoc to IExportManager & ExportSchema

--- electron/main/application/exporting/IExportManager.ts
+++ electron/main/application/exporting/IExportManager.ts
@@ -1,9 +1,36 @@
 export type ExportSchema = {
+  /**
+   * The relative export path
+   */
   path: string
+
+  /**
+   * A function that exports something given a path
+   *
+   * @param dest The full destination path
+   */
   callback: (dest: string) => void
 }
 
 export interface IExportManager {
+  /**
+   * Starts export pipeline by calling export schemas
+   *
+   * @param dest The folder path. Will be passed to export schemas
+   */
   export(dest: string): void
+
+  /**
+   * Add an export schema
+   *
+   * @param exportSchema The export schema
+   */
   addExportSchema(exportSchema: ExportSchema): void
+
+  /**
+   * Add multiple export schemas
+   *
+   * @param exportSchemas The export schemas
+   */
+  addExportSchemas(exportSchemas: ExportSchema[]): void
 }
