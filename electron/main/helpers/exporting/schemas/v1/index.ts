858308ec47ad1c8845d9ab09628c3d87e50167f0
Twaish <axiomeverything@gmail.com>
2026-03-12 21:42:08 +0100

    Add todos in exportSchemaV1

--- electron/main/helpers/exporting/schemas/v1/index.ts
+++ electron/main/helpers/exporting/schemas/v1/index.ts
@@ -1,6 +1,19 @@
 import { Modules } from '@/helpers/ipc/types'
 import { at, json, jsonStream } from '../../exporting-utils'
 
+/*
+TODO: Implement stream functions for all repositories
+TODO: Implement the rest of the streams
+await this.exportArray("rules", this.ruleRepository.streamRules())
+await this.exportArray("ruleEvents", this.ruleRepository.streamEvents())
+await this.exportArray("templates", this.templateRepository.streamAll())
+await this.exportArray("media", this.mediaRepository.streamAll())
+await this.exportArray("mediaGenres", this.mediaRepository.streamGenres())
+await writer.exportArray('mediaEmbeddings', this.mediaEmbeddingRepository.streamAll())
+await this.exportArray("watchPlans", this.watchPlanRepository.streamAll())
+await this.exportArray("watchPlanSegments", this.watchPlanRepository.streamSegments())
+await writer.exportArray('genres', this.genresRepository.streamAll())
+*/
 export const exportSchemaV1 = ({
   StorageService,
   GenresRepository,
