b2daca04f5ce453453e98f39549d08e388011930
Twaish <axiomeverything@gmail.com>
2026-03-12 17:37:41 +0100

    Implement addExportSchemas. Ensure relative paths are created when exporting

--- electron/main/infrastructure/exporting/ExportManager.ts
+++ electron/main/infrastructure/exporting/ExportManager.ts
@@ -3,6 +3,7 @@ import {
   IExportManager,
 } from '@/application/exporting/IExportManager'
 import path from 'path'
+import fs from 'fs'
 
 export class ExportManager implements IExportManager {
   constructor(private exportSchemas: ExportSchema[] = []) {}
@@ -11,9 +12,19 @@ export class ExportManager implements IExportManager {
     this.exportSchemas.push(exportSchema)
   }
 
+  addExportSchemas(exportSchemas: ExportSchema[]): void {
+    this.exportSchemas.push(...exportSchemas)
+  }
+
   export(dest: string): void {
-    this.exportSchemas.forEach((schema) =>
-      schema.callback(path.join(dest, schema.path)),
-    )
+    fs.mkdirSync(dest, { recursive: true })
+
+    this.exportSchemas.forEach((schema) => {
+      const schemaDest = path.join(dest, schema.path)
+
+      fs.mkdirSync(schemaDest, { recursive: true })
+
+      schema.callback(schemaDest)
+    })
   }
 }
