7f917aaaf9025d37552d69ea6cc581aa22b1a64c
Twaish <axiomeverything@gmail.com>
2026-03-15 21:26:39 +0100

    Pass context to next call of executeTemplate for ActionServices

--- electron/main/helpers/automation/create-action-services.ts
+++ electron/main/helpers/automation/create-action-services.ts
@@ -1,10 +1,13 @@
 import { RuleEngine } from '@/domain/automation/RuleEngine'
-import { ActionServices } from '@/domain/automation/types'
+import { ActionServices, RuleContext } from '@/domain/automation/types'
 
 export function createActionServices(ruleEngine: RuleEngine): ActionServices {
   return {
     callPlugin: async (name, args) => console.log(name, args),
-    callTemplate: async (name, args: Record<string, unknown>): Promise<void> =>
-      await ruleEngine.executeTemplate(name, args),
+    callTemplate: async (
+      name,
+      args: Record<string, unknown>,
+      context: RuleContext<Record<string, unknown>>,
+    ): Promise<void> => await ruleEngine.executeTemplate(name, args, context),
   }
 }
