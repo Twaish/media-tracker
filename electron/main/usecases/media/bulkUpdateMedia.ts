193854ed37f862e3a26150e0116519bbdec6d9e1
Twaish <axiomeverything@gmail.com>
2026-03-15 11:35:08 +0100

    Emit mediaUpdated event in BulkUpdateMedia use case

--- electron/main/usecases/media/bulkUpdateMedia.ts
+++ electron/main/usecases/media/bulkUpdateMedia.ts
@@ -1,11 +1,28 @@
 import { IMediaRepository } from '@/application/db/repositories/IMediaRepository'
 import { BulkUpdateMediaDTO } from '@shared/types'
+import { MEDIA_EVENTS, MediaUpdatedPayload } from './media.events'
+import { IEventBus } from '@/application/events/IEventBus'
 
-// TODO: Publish media updated event for all updated medias
 export default class BulkUpdateMedia {
-  constructor(private readonly repo: IMediaRepository) {}
+  constructor(
+    private readonly repo: IMediaRepository,
+    private readonly eventBus: IEventBus,
+  ) {}
 
   async execute(mediaUpdates: BulkUpdateMediaDTO) {
-    return this.repo.bulkUpdate(mediaUpdates)
+    const previous = await this.repo.getByIds(mediaUpdates.ids)
+
+    const updateResult = await this.repo.bulkUpdate(mediaUpdates)
+
+    const current = await this.repo.getByIds(updateResult.ids)
+
+    for (let i = 0; i < current.length; i++) {
+      this.eventBus.publish<MediaUpdatedPayload>(MEDIA_EVENTS.MEDIA_UPDATED, {
+        previous: previous[i],
+        current: current[i],
+      })
+    }
+
+    return updateResult
   }
 }
