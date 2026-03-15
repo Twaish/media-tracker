70634ab47b84a827280cb61215b9c64b73813e93
Twaish <axiomeverything@gmail.com>
2026-03-15 21:44:24 +0100

    Expose SyncRuleEngine use case to electron IPC object

--- electron/main/helpers/ipc/automation/automation-listeners.ts
+++ electron/main/helpers/ipc/automation/automation-listeners.ts
@@ -2,6 +2,7 @@ import { createAutomationUseCases } from '@/usecases/automation'
 import { Modules } from '../types'
 import {
   RULE_ADD,
+  RULE_ENGINE_SYNC,
   RULE_GET_ENABLED,
   RULE_REMOVE,
   RULE_UPDATE,
@@ -17,6 +18,7 @@ export function addAutomationEventListeners(modules: Modules) {
   const useCases = createAutomationUseCases(modules)
 
   registerIpcHandlers<AutomationContext>({
+    syncEngine: [RULE_ENGINE_SYNC, () => useCases.syncRuleEngine.execute()],
     addRule: [RULE_ADD, (_, rule) => useCases.addRule.execute(rule)],
     updateRule: [RULE_UPDATE, (_, rule) => useCases.updateRule.execute(rule)],
     removeRules: [RULE_REMOVE, (_, ids) => useCases.removeRules.execute(ids)],
