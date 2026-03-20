6544ae0112cdc90bd8e339697706b86dd5e28482
Twaish <axiomeverything@gmail.com>
2026-03-20 10:59:12 +0100

    Add missing importLibrary to shared ExportingContext type

--- shared/types/exporting.d.ts
+++ shared/types/exporting.d.ts
@@ -1,3 +1,6 @@
+import { ImportResult } from '@/application/exporting/IImportManager'
+
 export interface ExportingContext {
   exportLibrary(path: string): Promise<void>
+  importLibrary(path: string): Promise<ImportResult>
 }
