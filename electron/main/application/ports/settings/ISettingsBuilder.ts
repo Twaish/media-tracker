0fc38de2dccdc79663e7ed8a7fed4dbb1b086909
Twaish <axiomeverything@gmail.com>
2026-03-25 01:36:22 +0100

    Add ISettingsBuilder

---
+++ electron/main/application/ports/settings/ISettingsBuilder.ts
@@ -0,0 +1,38 @@
+export type Schema = Record<string, { default?: any; secret?: boolean }>
+
+export type OnDiskValue<T> = 'default' extends keyof T
+  ? T['default']
+  : T extends { secret: true }
+    ? { __encrypted: true; value: string }
+    : unknown
+
+export type OnDiskSchema<T extends Schema> = {
+  [K in keyof T]: OnDiskValue<T[K]>
+}
+
+export type RuntimeValue<T> = 'default' extends keyof T
+  ? T['default']
+  : T extends { secret: true }
+    ? string | undefined
+    : unknown
+
+export type RuntimeSchema<T extends Schema> = {
+  [K in keyof T]: RuntimeValue<T[K]>
+}
+
+export type SettingsInterface<T extends Schema = {}> = {
+  namespace: string
+  init(): Promise<void>
+  get<K extends keyof T>(key: K): RuntimeSchema<T>[K]
+  set<K extends keyof T>(key: K, value: RuntimeSchema<T>[K]): Promise<void>
+  getAll(): RuntimeSchema<T>
+  isSecret(key: keyof T): boolean
+}
+
+export interface ISettingsBuilder {
+  defineSettings<T extends Schema>(
+    namespace: string,
+    filename: string,
+    schema: T,
+  ): SettingsInterface<T>
+}
