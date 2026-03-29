4dc49d4337b4bdcb74689ffaf9ce2cfd5b0539b8
Twaish <axiomeverything@gmail.com>
2026-03-29 21:24:51 +0200

    Add ISettingsRegistry

---
+++ electron/main/application/ports/settings/ISettingsRegistry.ts
@@ -0,0 +1,12 @@
+import { RuntimeSchema, SettingsInterface } from './ISettingsBuilder'
+
+export interface ISettingsRegistry {
+  register(namespace: string, provider: SettingsInterface): void
+
+  get(
+    namespace: string,
+    key: keyof RuntimeSchema,
+  ): RuntimeSchema[keyof RuntimeSchema]
+
+  getSecret(namespace: string, key: string): string
+}
