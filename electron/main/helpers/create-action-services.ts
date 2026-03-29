b4c43907363b6670c7586b9b2afcaf8136772a2e
Twaish <axiomeverything@gmail.com>
2026-03-30 00:45:33 +0200

    Add plugin manager to action services

--- electron/main/helpers/create-action-services.ts
+++ electron/main/helpers/create-action-services.ts
@@ -1,10 +1,16 @@
+import { IPluginManager } from '@/application/ports/plugins/IPluginManager'
 import { IRuleEngine } from '@/features/automation/application/interfaces/IRuleEngine'
 import { RuleContext } from '@/features/automation/domain/runtime/RuleContext'
 import { ActionServices } from '@/features/automation/domain/services/ActionServices'
 
-export function createActionServices(ruleEngine: IRuleEngine): ActionServices {
+export function createActionServices(
+  ruleEngine: IRuleEngine,
+  pluginManager: IPluginManager,
+): ActionServices {
   return {
-    callPlugin: async (name, args) => console.log(name, args),
+    callPlugin: async (name, ...args: unknown[]): Promise<void> => {
+      await pluginManager.execute(name, ...args)
+    },
     callTemplate: async (
       name,
       args: Record<string, unknown>,
