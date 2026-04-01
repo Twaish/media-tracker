4a44c8bf51b6a56a709450c59715707fcdbfdcfd
Twaish <axiomeverything@gmail.com>
2026-04-01 19:32:03 +0200

    Add function to create storage routers

--- electron/main/features/storage/ipc/index.ts
+++ electron/main/features/storage/ipc/index.ts
@@ -1,2 +1,27 @@
+import { os } from '@orpc/server'
+
+import { Modules } from '@/helpers/ipc/types'
+import { createStorageUseCases } from '../usecases'
+
+import {
+  exportImagesInputSchema,
+  storedImageResultSchema,
+  storeImageInputSchema,
+} from './schemas'
+
 export * from './storage-channels'
 export * from './storage-listeners'
+
+export function createStorageRouters(modules: Modules) {
+  const useCases = createStorageUseCases(modules)
+
+  return {
+    storeImage: os
+      .input(storeImageInputSchema)
+      .output(storedImageResultSchema)
+      .handler(({ input }) => useCases.storeImage.execute(input)),
+    exportImages: os
+      .input(exportImagesInputSchema)
+      .handler(({ input }) => useCases.exportImages.execute(input)),
+  }
+}
