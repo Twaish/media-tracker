d9b5a4d9350f494b7771c8646b76d6193d2304b2
Twaish <axiomeverything@gmail.com>
2026-04-04 23:16:08 +0200

    Update plugin example

--- examples/TestPlugin/index.js
+++ examples/TestPlugin/index.js
@@ -8,25 +8,40 @@ const state = {}
 export default {
   state,
 
-  setup(modules, pluginDir) {
-    modules.logger.info(`Setting up plugin "${pkg.name}" at ${pluginDir}`)
-    this.state.pluginDir = pluginDir
+  getSettings() {
+    return {
+      callCount: { default: 0 },
+      encryptedCount: { default: 0, secret: true },
+    }
   },
 
-  async execute(modules, ...args) {
-    const { pluginDir } = this.state
+  async setup(modules, pluginDir) {
+    modules.logger.info(`Setting up plugin "${pkg.name}" at ${pluginDir}`)
 
     const outDir = path.join(pluginDir, outputDir)
     await fs.mkdir(outDir, { recursive: true })
 
     const filePath = path.join(outDir, 'args.json')
-    const fileContent = JSON.stringify({ arguments: args })
 
-    await fs.writeFile(filePath, fileContent)
+    this.state = {
+      pluginDir,
+      settings,
+      outDir,
+      filePath,
+    }
+  },
 
+  async execute(modules, ...args) {
     modules.logger.info(`Executing plugin "${pkg.name}"`)
+    const { filePath, settings } = this.state
 
+    await fs.writeFile(filePath, JSON.stringify({ arguments: args }))
     modules.logger.info(`Writing arguments to file at ${filePath}`)
+
+    const newCount = settings.get('callCount') + 1
+    await settings.set('callCount', newCount)
+    await settings.set('encryptedCount', '' + newCount)
+    modules.logger.info(`${pkg.name} has been executed ${newCount} times`)
   },
 
   destroy(modules) {
