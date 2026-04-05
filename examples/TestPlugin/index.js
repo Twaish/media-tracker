4b6ca48e6f0710fad4d596b61ed28acc6d7d05a4
Twaish <axiomeverything@gmail.com>
2026-04-05 19:07:41 +0200

    Update plugin example

--- examples/TestPlugin/index.js
+++ examples/TestPlugin/index.js
@@ -1,50 +1,62 @@
-import pkg from './manifest.json' with { type: 'json' }
-import fs from 'fs/promises'
-import path from 'path'
+const pkg = require('./manifest.json')
+const path = require('path')
+const fs = require('fs/promises')
 
-const outputDir = 'out'
-const state = {}
+const OUTPUT_DIR = 'out'
 
-export default {
-  state,
+module.exports = {
+  state: {},
 
-  getSettings() {
-    return {
-      callCount: { default: 0 },
-      encryptedCount: { default: 0, secret: true },
-    }
+  settings: {
+    callCount: { default: 0 },
+    encryptedCount: { default: 0, secret: true },
   },
 
-  async setup(modules, pluginDir) {
-    modules.logger.info(`Setting up plugin "${pkg.name}" at ${pluginDir}`)
+  async setup(ctx) {
+    const { pluginDir, events, logger } = ctx
+    const eventName = `plugin:${pkg.name}`
 
-    const outDir = path.join(pluginDir, outputDir)
+    logger.info(`Setting up plugin "${pkg.name}" at ${pluginDir}`)
+
+    const outDir = path.join(pluginDir, OUTPUT_DIR)
     await fs.mkdir(outDir, { recursive: true })
 
     const filePath = path.join(outDir, 'args.json')
 
+    const unsubscribe = events.subscribe(eventName, (value) => {
+      logger.debug(`[${eventName}] ${JSON.stringify(value, null, 2)}`)
+    })
+
     this.state = {
-      pluginDir,
-      settings,
-      outDir,
       filePath,
+      eventName,
+      cleanup: [unsubscribe],
     }
   },
 
-  async execute(modules, ...args) {
-    modules.logger.info(`Executing plugin "${pkg.name}"`)
-    const { filePath, settings } = this.state
+  async execute(ctx, ...args) {
+    const { settings, events, logger } = ctx
+    const { filePath, eventName } = this.state
 
-    await fs.writeFile(filePath, JSON.stringify({ arguments: args }))
-    modules.logger.info(`Writing arguments to file at ${filePath}`)
+    logger.info(`Executing plugin "${pkg.name}"`)
+
+    const data = {
+      arguments: args,
+      timestamp: new Date().toISOString(),
+    }
+
+    await fs.writeFile(path.join(filePath), JSON.stringify(data, null, 2))
+    logger.info(`Wrote arguments to ${filePath}`)
 
     const newCount = settings.get('callCount') + 1
-    await settings.set('callCount', newCount)
-    await settings.set('encryptedCount', '' + newCount)
-    modules.logger.info(`${pkg.name} has been executed ${newCount} times`)
+    settings.set('callCount', newCount)
+    settings.set('encryptedCount', String(newCount))
+
+    events.emit(eventName, `${pkg.name} has been executed ${newCount} times`)
   },
 
-  destroy(modules) {
-    modules.logger.warn(`Destroying plugin "${pkg.name}"`)
+  destroy(ctx) {
+    ctx.logger.warn(`Destroying plugin "${pkg.name}"`)
+    this.state.cleanup.forEach((c) => c())
   },
 }
