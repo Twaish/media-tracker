916e99aae36b43d73a112bb0c0b903ceaad4d450
Twaish <axiomeverything@gmail.com>
2026-04-10 18:52:17 +0200

    Fetch images from remote urls

--- electron/main/helpers/ipc/protocols-register.ts
+++ electron/main/helpers/ipc/protocols-register.ts
@@ -1,11 +1,20 @@
 import { net, protocol } from 'electron'
 import { Modules } from './types'
 
+function isRemoteUrl(path: string) {
+  return path.startsWith('http://') || path.startsWith('https://')
+}
+
 export function registerProtocols({ StorageService }: Modules) {
   protocol.handle('images', function (request) {
-    const imagePath = StorageService.resolve(
-      request.url.slice('images://'.length),
-    )
+    const url = new URL(request.url)
+    const remoteUrl = url.searchParams.get('url')
+
+    if (remoteUrl && isRemoteUrl(remoteUrl)) {
+      return net.fetch(remoteUrl)
+    }
+
+    const imagePath = StorageService.resolve(url.hostname)
     return net.fetch(`file://${imagePath}`)
   })
 }
