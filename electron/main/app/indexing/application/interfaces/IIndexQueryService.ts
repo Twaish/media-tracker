c568194d5e6fed94c397dfb40b93c279a20fed3d
Twaish <axiomeverything@gmail.com>
2026-04-11 01:19:31 +0200

    Add example to .search() in IIndexQueryService

--- electron/main/app/indexing/application/interfaces/IIndexQueryService.ts
+++ electron/main/app/indexing/application/interfaces/IIndexQueryService.ts
@@ -6,7 +6,11 @@ export type IndexSearchResult = {
 export interface IIndexQueryService {
   /**
    * Search index files
-   *
+   * ```
+   * const result = await indexQueryService.search("One Punch Man")
+   * // or using only select index files
+   * await indexQueryService.search("One Punch Man", ["anime-db", ...])
+   * ```
    * @param query The text to search for
    * @param ids The ids for which index files to use
    */
@@ -17,7 +21,6 @@ export interface IIndexQueryService {
 
   /**
    * Get the full entry from an index file by its id and index in the entry array
-   *
    * @param id The id for the index file
    * @param index The index for the entry to retrieve
    */
