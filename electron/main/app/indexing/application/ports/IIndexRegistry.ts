135ca7372807a8e2b31a20cba86caa3f9b5e6b99
Twaish <axiomeverything@gmail.com>
2026-04-11 01:02:55 +0200

    Remove .search() and .getEntry() from IIndexRegistry

--- electron/main/app/indexing/application/ports/IIndexRegistry.ts
+++ electron/main/app/indexing/application/ports/IIndexRegistry.ts
@@ -11,23 +11,4 @@ export interface IIndexRegistry {
 
   get(id: string): IndexFileManifest
   getAll(): IndexFileManifest[]
-
-  /**
-   * Search index files
-   *
-   * @param query The text to search for
-   * @param ids The ids for which index files to use
-   */
-  search(
-    query: string,
-    ids?: string[],
-  ): Promise<Record<string, IndexSearchResult[]>>
-
-  /**
-   * Get the full entry from an index file by its id and index in the entry array
-   *
-   * @param id The id for the index file
-   * @param index The index for the entry to retrieve
-   */
-  getEntry(id: string, index: number): Promise<Record<string, unknown> | null>
 }
