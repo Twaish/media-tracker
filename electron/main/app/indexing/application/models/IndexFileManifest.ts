36a9870f2a947204e0fbad6ab3dedaa5b92145b4
Twaish <axiomeverything@gmail.com>
2026-04-12 00:17:08 +0200

    Add example and make .entriesPath optional for IndexExtractionSchema. Make source required in IndexFileManifest

--- electron/main/app/indexing/application/models/IndexFileManifest.ts
+++ electron/main/app/indexing/application/models/IndexFileManifest.ts
@@ -1,5 +1,28 @@
+/**
+ * Determines how to access entries at the import/refresh stage and other properties.
+ *
+ * Using the following schema
+ * ```
+ * { entriesPath: "nested.data", title: ["title", "synonyms"] }
+ * ```
+ * in an index file like this:
+ * ```
+ * {
+ *   "nested": { "data": [ { title: "One Punch Man", synonyms: ["OPM"] }, ...] }
+ * }
+ * ```
+ * would result in the following .jsonl file
+ * ```
+ * { title: "One Punch Man", synonyms: ["OPM"] }
+ * { title: ... }
+ * ```
+ * and data looking like this when extracted
+ * ```
+ * ["One Punch Man", "OPM"]
+ * ```
+ */
 export type IndexExtractionSchema = {
-  entriesPath: string // Key path to list of entries
+  entriesPath?: string // Key path to list of entries (only used at importing/refresh stage)
   title: string | string[] // Keys for which values to search for every entry (title, alternate titles)
 }
 
@@ -7,7 +30,7 @@ export type IndexFileManifest = {
   id: string
   name: string
   filePath: string
-  source?: string
+  source: string
   version?: string
   importedAt: Date
   lastModified?: Date // source last modified
