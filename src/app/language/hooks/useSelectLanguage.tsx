c73eb3ba74dff9e7266dfe1cbce0e0b29b9d72ce
Twaish <axiomeverything@gmail.com>
2026-04-26 14:55:11 +0200

    Add non-hook selectLanguage to open modal outside React components

--- src/app/language/hooks/useSelectLanguage.tsx
+++ src/app/language/hooks/useSelectLanguage.tsx
@@ -1,8 +1,14 @@
 import { useModalStore } from '@/stores/modal/useModalStore'
-import { useTranslation } from 'react-i18next'
 import { setAppLanguage } from '../actions'
 import { SelectLanguageDialog } from '../components/SelectLanguageDialog'
 import { Language } from '../language'
+import i18n from 'i18next'
+
+const createHandleSelect =
+  (onSelect?: (language: Language) => void) => (value: Language) => {
+    setAppLanguage(value.key, i18n)
+    onSelect?.(value)
+  }
 
 interface UseSelectLanguageOptions {
   onSelect?: (lang: Language) => void
@@ -10,16 +16,16 @@ interface UseSelectLanguageOptions {
 
 export function useSelectLanguage({ onSelect }: UseSelectLanguageOptions = {}) {
   const open = useModalStore((s) => s.open)
-  const { i18n } = useTranslation()
-
-  const handleSelect = (value: Language) => {
-    setAppLanguage(value.key, i18n)
-    onSelect?.(value)
-  }
 
   const selectLanguage = () => {
-    open(<SelectLanguageDialog onSelect={handleSelect} />)
+    open(<SelectLanguageDialog onSelect={createHandleSelect(onSelect)} />)
   }
 
   return { selectLanguage }
+}
+
+export const selectLanguage = ({ onSelect }: UseSelectLanguageOptions = {}) => {
+  useModalStore
+    .getState()
+    .open(<SelectLanguageDialog onSelect={createHandleSelect(onSelect)} />)
 }
