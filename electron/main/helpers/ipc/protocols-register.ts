59961b0fff127ded80477cf13571f40c5aa82197
Twaish <axiomeverything@gmail.com>
2026-04-30 09:50:11 +0200

    Add specific protocols for fetching remote images, theme icons, and media images

--- electron/main/helpers/ipc/protocols-register.ts
+++ electron/main/helpers/ipc/protocols-register.ts
@@ -1,20 +1,82 @@
 import { net, protocol } from 'electron'
-import { Modules } from './types'
+import { pathToFileURL } from 'url'
 
 function isRemoteUrl(path: string) {
   return path.startsWith('http://') || path.startsWith('https://')
 }
 
-export function registerProtocols({ StorageService }: Modules) {
-  protocol.handle('images', (request) => {
+const createProtocolHandler =
+  ({
+    resolve,
+    validate,
+    toFetchUrl = (v) => pathToFileURL(v).toString(),
+  }: {
+    resolve: (url: URL) => string | null
+    validate?: (url: URL) => boolean
+    toFetchUrl?: (resolved: string) => string
+  }) =>
+  (request: Request) => {
     const url = new URL(request.url)
-    const remoteUrl = url.searchParams.get('url')
-
-    if (remoteUrl && isRemoteUrl(remoteUrl)) {
-      return net.fetch(remoteUrl)
+    if (validate && !validate(url)) {
+      return new Response('Bad Request', { status: 400 })
     }
 
-    const imagePath = StorageService.resolve(url.hostname)
-    return net.fetch(`file://${imagePath}`)
-  })
+    const resolved = resolve(url)
+    if (!resolved) {
+      throw new Response('Forbidden', { status: 403 })
+    }
+
+    return net.fetch(toFetchUrl(resolved))
+  }
+
+protocol.registerSchemesAsPrivileged([
+  {
+    scheme: 'images',
+    privileges: { secure: true, standard: true, supportFetchAPI: true },
+  },
+  {
+    scheme: 'themeicon',
+    privileges: { secure: true, standard: true, supportFetchAPI: true },
+  },
+  {
+    scheme: 'remoteimage',
+    privileges: {
+      secure: true,
+      standard: true,
+      supportFetchAPI: true,
+      corsEnabled: true,
+    },
+  },
+])
+
+export function registerProtocols({
+  resolveMediaThumbnail,
+  resolveThemeIcon,
+}: {
+  resolveMediaThumbnail: (path: string) => string | null
+  resolveThemeIcon: (path: string) => string | null
+}) {
+  protocol.handle(
+    'images',
+    createProtocolHandler({
+      resolve: (url) => resolveMediaThumbnail(url.hostname),
+    }),
+  )
+  protocol.handle(
+    'themeicon',
+    createProtocolHandler({
+      resolve: (url) => resolveThemeIcon(url.hostname),
+    }),
+  )
+  protocol.handle(
+    'remoteimage',
+    createProtocolHandler({
+      validate: (url) => {
+        const remoteUrl = url.searchParams.get('url')
+        return !!remoteUrl && isRemoteUrl(remoteUrl)
+      },
+      resolve: (url) => url.searchParams.get('url'),
+      toFetchUrl: (url) => url,
+    }),
+  )
 }
