c45101ccd6b7a06ffcddede4521d4bd7bf2f514e
Twaish <axiomeverything@gmail.com>
2026-04-30 09:43:31 +0200

    Add themeTokens as shared constant. Add shared ThemeToken, ThemeTokenMap, and ThemeId types

--- shared/types/features/theme.d.ts
+++ shared/types/features/theme.d.ts
@@ -1,3 +1,7 @@
-import { themeModes } from '@shared/constants'
+import { themeModes, themeTokens } from '@shared/constants'
 
 export type ThemeMode = (typeof themeModes)[number]
+export type ThemeToken = (typeof themeTokens)[number]
+export type ThemeTokenMap = Partial<Record<ThemeToken, string>>
+
+export type ThemeId = ThemeMode | (string & {})
