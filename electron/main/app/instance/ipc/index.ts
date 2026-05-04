419771827a1f6ef4603dded61b66145b66226e63
Twaish <axiomeverything@gmail.com>
2026-05-04 11:38:43 +0200

    Expose openFolder and openLink methods through oRPC

--- electron/main/app/instance/ipc/index.ts
+++ electron/main/app/instance/ipc/index.ts
@@ -1,10 +1,25 @@
+import { shell } from 'electron'
+import fs from 'fs'
 import { os } from '@orpc/server'
 import { Modules } from '@/helpers/ipc/types'
-import { nameOutputSchema, versionOutputSchema } from './schemas'
+import {
+  nameOutputSchema,
+  openFolderInputSchema,
+  openLinkInputSchema,
+  versionOutputSchema,
+} from './schemas'
 
 export function createInstanceRouters({ appInfo }: Modules) {
   return {
     name: os.output(nameOutputSchema).handler(() => appInfo.name),
     version: os.output(versionOutputSchema).handler(() => appInfo.version),
+    openFolder: os.input(openFolderInputSchema).handler(({ input }) => {
+      if (fs.existsSync(input)) {
+        shell.openPath(input)
+      }
+    }),
+    openLink: os.input(openLinkInputSchema).handler(({ input }) => {
+      shell.openExternal(input)
+    }),
   }
 }
--- electron/main/app/instance/ipc/schemas.ts
+++ electron/main/app/instance/ipc/schemas.ts
@@ -3,3 +3,7 @@ import z from 'zod'
 export const nameOutputSchema = z.string()
 
 export const versionOutputSchema = z.string()
+
+export const openFolderInputSchema = z.string()
+
+export const openLinkInputSchema = z.string()
