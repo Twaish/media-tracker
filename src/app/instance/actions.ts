6400e09f6579c77ed480047d96c3f94786e7ca7d
Twaish <axiomeverything@gmail.com>
2026-05-04 11:39:18 +0200

    Add openFolder and openLink instance actions

--- src/app/instance/actions.ts
+++ src/app/instance/actions.ts
@@ -7,3 +7,11 @@ export function getAppName() {
 export function getAppVersion() {
   return ipc.client.instance.version()
 }
+
+export function openFolder(path: string) {
+  return ipc.client.instance.openFolder(path)
+}
+
+export function openLink(link: string) {
+  return ipc.client.instance.openLink(link)
+}
