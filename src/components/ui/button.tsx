b469aa16e5a0ca739b2ea5cfbc1170e476bd7662
Twaish <axiomeverything@gmail.com>
2026-05-06 15:40:27 +0200

    Add default rounded-sm to Button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/utils/tailwind'
 import { Rippler } from './button-rippler'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap text-xs font-medium ring-offset-background cursor-pointer transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative',
+  'inline-flex items-center justify-center rounded-sm whitespace-nowrap text-xs font-medium ring-offset-background cursor-pointer transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative',
   {
     variants: {
       variant: {
