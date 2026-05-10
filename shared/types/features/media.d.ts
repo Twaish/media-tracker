9f15e011d4e32f74cbbb88e280c7b9f65c923d12
Twaish <axiomeverything@gmail.com>
2026-05-10 14:44:21 +0200

    Export shared PersistedMedia type

--- shared/types/features/media.d.ts
+++ shared/types/features/media.d.ts
@@ -3,3 +3,5 @@ export type {
   UpdateMediaDTO,
   BulkUpdateMediaDTO,
 } from '@main/features/media/application/dto/media.dto'
+
+export type { PersistedMedia } from '@main/features/media/domain/entities/media'
