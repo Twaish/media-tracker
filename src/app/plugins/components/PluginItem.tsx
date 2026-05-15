a9c254f09cbcf44f8a011ac203daa95291121330
Twaish <axiomeverything@gmail.com>
2026-05-15 12:50:39 +0200

    Highlight plugin item text by query. Move PluginItem description and toggle to separate components

--- src/app/plugins/components/PluginItem.tsx
+++ src/app/plugins/components/PluginItem.tsx
@@ -12,30 +12,25 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip'
 import { TriangleAlert } from 'lucide-react'
+import { Highlight } from '@/components/Highlight'
 
-export function PluginItem({ plugin }: { plugin: Plugin }) {
-  const manifest = plugin.manifest
-  const refresh = usePluginStore((s) => s.refresh)
-
-  const handleCheckedChange = async (shouldEnable: boolean) => {
-    if (shouldEnable) {
-      await enablePlugin(manifest.id)
-      refresh()
-    } else {
-      await disablePlugin(manifest.id)
-      refresh()
-    }
-  }
-
+export function PluginItem({
+  plugin,
+  query,
+}: {
+  plugin: Plugin
+  query: string
+}) {
   return (
     <PluginItemContext.Provider
       value={{
+        query,
         plugin,
         manifest: plugin.manifest,
-        namespace: `plugin:${manifest.id}`,
+        namespace: `plugin:${plugin.manifest.id}`,
       }}
     >
-      <div className="bg-card flex-1 border p-2">
+      <div className="bg-card flex-1 border-t p-2">
         <PluginItem.Header>
           <PluginItem.Icon />
           <PluginItem.Details>
@@ -45,41 +40,9 @@ export function PluginItem({ plugin }: { plugin: Plugin }) {
             <PluginItem.Author />
           </PluginItem.Details>
           <PluginDialog />
-          <Switch
-            className="self-start"
-            title={plugin.enabled ? 'enabled' : 'disabled'}
-            checked={plugin.enabled}
-            disabled={plugin.error != null}
-            onCheckedChange={handleCheckedChange}
-          />
+          <PluginItem.Toggle />
         </PluginItem.Header>
-
-        <p className="text-muted-foreground mr-auto line-clamp-2 flex-1 text-xs">
-          {manifest.description ?? 'This plugin has no description'}
-        </p>
-        {manifest.permissions && (
-          <div className="mt-2 flex flex-wrap items-center gap-1">
-            {manifest.permissions.slice(0, 3).map((perm) => (
-              <PluginItem.Permission key={perm}>{perm}</PluginItem.Permission>
-            ))}
-            {manifest.permissions.length > 3 && (
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <span className="text-accent-foreground cursor-pointer text-[10px]">
-                    +{manifest.permissions.length - 3} more
-                  </span>
-                </TooltipTrigger>
-                <TooltipContent className="flex flex-col gap-1">
-                  {manifest.permissions.slice(3).map((perm) => (
-                    <span key={perm} className="text-xs">
-                      {perm}
-                    </span>
-                  ))}
-                </TooltipContent>
-              </Tooltip>
-            )}
-          </div>
-        )}
+        <PluginItem.Description />
       </div>
     </PluginItemContext.Provider>
   )
@@ -102,6 +65,38 @@ PluginItem.Header = function Header({
     </div>
   )
 }
+PluginItem.Toggle = function Toggle() {
+  const { plugin, manifest } = usePluginItem()
+  const refresh = usePluginStore((s) => s.refresh)
+
+  const handleCheckedChange = async (shouldEnable: boolean) => {
+    if (shouldEnable) {
+      await enablePlugin(manifest.id)
+      refresh()
+    } else {
+      await disablePlugin(manifest.id)
+      refresh()
+    }
+  }
+
+  return (
+    <Switch
+      className="self-start"
+      title={plugin.enabled ? 'enabled' : 'disabled'}
+      checked={plugin.enabled}
+      disabled={plugin.error != null}
+      onCheckedChange={handleCheckedChange}
+    />
+  )
+}
+PluginItem.Description = function Description() {
+  const { manifest } = usePluginItem()
+  return (
+    <p className="text-muted-foreground mr-auto line-clamp-2 flex-1 text-xs">
+      {manifest.description ?? 'This plugin has no description'}
+    </p>
+  )
+}
 PluginItem.Icon = function Icon() {
   const { manifest } = usePluginItem()
   return manifest.icon ? (
@@ -114,10 +109,12 @@ PluginItem.Details = function Details({ children }: { children?: ReactNode }) {
   return <div className="flex-1 overflow-hidden">{children}</div>
 }
 PluginItem.Title = function Title({ children }: { children?: ReactNode }) {
-  const { manifest } = usePluginItem()
+  const { manifest, query } = usePluginItem()
   return (
     <div className="flex items-center gap-2">
-      <h3 className="truncate text-sm font-medium">{manifest.name}</h3>
+      <h3 className="truncate text-sm font-medium">
+        <Highlight text={manifest.name} term={query} />
+      </h3>
       {manifest.version && (
         <span className="text-muted-foreground shrink-0 font-mono text-[10px]">
           v{manifest.version}
@@ -132,7 +129,7 @@ PluginItem.Author = function Author({
   className,
   ...rest
 }: ComponentProps<'p'> & { children?: ReactNode }) {
-  const { manifest } = usePluginItem()
+  const { manifest, query } = usePluginItem()
   return (
     <p
       className={cn(
@@ -141,26 +138,16 @@ PluginItem.Author = function Author({
       )}
       {...rest}
     >
-      <span>by {manifest.author ?? 'Unknown'}</span>
+      <span>
+        by{' '}
+        {manifest.author ? (
+          <Highlight text={manifest.author} term={query} />
+        ) : (
+          'Unknown'
+        )}
+      </span>
       {children}
     </p>
-  )
-}
-PluginItem.Permission = function Permission({
-  className,
-  children,
-  ...rest
-}: ComponentProps<'div'>) {
-  return (
-    <div
-      className={cn(
-        'bg-muted text-muted-foreground flex h-5 items-center px-1.5 text-[10px]',
-        className,
-      )}
-      {...rest}
-    >
-      {children}
-    </div>
   )
 }
 PluginItem.Error = function Error({
