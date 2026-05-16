7cf674258ef41a035aaf1bc6c7de26ba6727fd38
Twaish <axiomeverything@gmail.com>
2026-05-16 23:53:39 +0200

    Fetch media entry in MediaPreview instead of using media store
    - Media store holds medias for only the current page, so next media for previews might not exist in the store resulting in no previews

--- src/features/media/components/MediaPreview.tsx
+++ src/features/media/components/MediaPreview.tsx
@@ -1,34 +1,30 @@
 import { ComponentProps } from 'react'
 import { MediaThumbnail } from './MediaThumbnail'
-import { selectMedia, useMediaStore } from '../stores/mediaStore'
 import { cn } from '@/utils/tailwind'
+import { useQuery } from '@tanstack/react-query'
+import { getMediaByIdQueryOptions } from '../queries'
 
 export function MediaPreview({
   mediaId,
   className,
   children,
   ...props
-}: { mediaId?: number | null } & ComponentProps<'div'>) {
-  const nextMedia = useMediaStore((s) =>
-    mediaId ? selectMedia(mediaId)(s) : null,
-  )
-  if (!nextMedia) return
+}: { mediaId: number } & ComponentProps<'div'>) {
+  const { data } = useQuery(getMediaByIdQueryOptions(mediaId))
+  if (!data) return
 
   return (
     <div className={cn('flex gap-2', className)} {...props}>
-      <MediaThumbnail
-        className="h-20 w-14 rounded-md"
-        src={nextMedia.thumbnail}
-      />
+      <MediaThumbnail className="h-20 w-14 rounded-md" src={data.thumbnail} />
       <div className="flex min-w-0 flex-1 flex-col">
         <div className="flex items-start justify-between gap-2">
           <div className="line-clamp-2 text-sm leading-tight font-medium">
-            {nextMedia.title}
+            {data.title}
           </div>
         </div>
         <div className="text-muted-foreground flex text-xs">
-          Episode {nextMedia.currentEpisode}{' '}
-          {nextMedia.maxEpisodes && `/ ${nextMedia.maxEpisodes}`}
+          Episode {data.currentEpisode}{' '}
+          {data.maxEpisodes && `/ ${data.maxEpisodes}`}
         </div>
         {children}
       </div>
