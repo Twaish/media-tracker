a6680cef3b2999ced57a4f1ca5e6678ee6ba8fee
Twaish <axiomeverything@gmail.com>
2026-05-16 23:54:56 +0200

    Make MediaFavoriteButton modular
    - Update side effect when toggling the favorite button. It now takes a `checked` boolean and state is handled elsewhere

--- src/features/media/components/MediaFavoriteButton.tsx
+++ src/features/media/components/MediaFavoriteButton.tsx
@@ -1,31 +1,18 @@
+import { ComponentProps } from 'react'
 import { Heart } from 'lucide-react'
-import { selectMedia, useMediaStore } from '../stores/mediaStore'
-import { useMediaInfo } from '../contexts/useMediaInfo'
 import { cn } from '@/utils/tailwind'
-import { ComponentProps, MouseEvent } from 'react'
 import { MediaIconButton } from './MediaIconButton'
 
 export function MediaFavoriteButton({
-  onClick,
+  checked,
   ...props
-}: ComponentProps<'button'>) {
-  const { id } = useMediaInfo()
-  const isFavorite = useMediaStore((s) => selectMedia(id)(s).isFavorite)
-  const updateMedia = useMediaStore((s) => s.updateMedia)
-
-  const handleFavoriteToggle = (
-    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
-  ) => {
-    updateMedia(id, { isFavorite: !isFavorite })
-    onClick?.(e)
-  }
-
+}: ComponentProps<'button'> & { checked?: boolean }) {
   return (
-    <MediaIconButton onClick={handleFavoriteToggle} {...props}>
+    <MediaIconButton {...props}>
       <Heart
         className={cn(
           'h-4 w-4 transition-colors',
-          isFavorite && 'fill-red-500 text-red-500',
+          checked && 'fill-red-500 text-red-500',
         )}
       />
     </MediaIconButton>
