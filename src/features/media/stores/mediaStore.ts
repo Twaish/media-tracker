dc8369b2bf192bdfd47c2c78b89dc1d4b93764f5
Twaish <axiomeverything@gmail.com>
2026-05-17 22:16:55 +0200

    Remove unused updateMedia and add setMedia to mediaStore

--- src/features/media/stores/mediaStore.ts
+++ src/features/media/stores/mediaStore.ts
@@ -1,12 +1,11 @@
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 import { PersistedMedia } from '@shared/types'
-import { updateMedia as updateMediaAction } from '../actions'
 
 interface MediaState {
   media: Record<string, PersistedMedia>
   setMedias(entries: PersistedMedia[]): void
-  updateMedia(id: number, patch: Partial<PersistedMedia>): void
+  setMedia(entry: PersistedMedia): void
 }
 
 export const useMediaStore = create<MediaState>()(
@@ -22,26 +21,10 @@ export const useMediaStore = create<MediaState>()(
         }
       }),
 
-    updateMedia(id, patch) {
+    setMedia: (entry) =>
       set((state) => {
-        if (state.media[id]) {
-          Object.assign(state.media[id], patch)
-          state.media[id].lastUpdated = new Date()
-        }
-      })
-
-      let genreIds = patch.genres?.map((g) => g.id)
-
-      const snapshot = useMediaStore.getState().media[id]
-
-      updateMediaAction({ id, ...patch, genres: genreIds }).catch(() => {
-        set((state) => {
-          if (state.media[id] && snapshot) {
-            state.media[id] = snapshot
-          }
-        })
-      })
-    },
+        state.media[entry.id] = entry
+      }),
   })),
 )
 
@@ -51,4 +34,4 @@ export const selectMedia = (id: number) => (state: MediaState) =>
 export const selectProp =
   <K extends keyof PersistedMedia>(id: number, property: K) =>
   (state: MediaState): PersistedMedia[K] =>
-    state.media[id][property]
+    state.media[id]?.[property]
