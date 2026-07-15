818fa99e8c0c6d54d60d959990336438d20ec1ba
Twaish <axiomeverything@gmail.com>
2026-07-15 22:20:35 +0200

    Export PersistedRule and PersistedTemplate types

--- shared/types/features/automation.d.ts
+++ shared/types/features/automation.d.ts
@@ -2,3 +2,5 @@ export type {
   AddNodeDTO,
   UpdateNodeDTO,
 } from '@main/features/automation/application/dto/automation.dto'
+export type { PersistedRule } from '@main/features/automation/domain/entities/rule'
+export type { PersistedTemplate } from '@main/features/automation/domain/entities/template'
